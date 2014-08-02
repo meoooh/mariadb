d3c39875bec4498f4febe0caddc2d449c7453196
han <hgk617@naver.com>
2014-08-02 19:15:07 +0900

    환경변수로 비밀번호 설정하면 echo 안함

--- create_account.sh
+++ create_account.sh
@@ -21,7 +21,12 @@ mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO '$ID'@'%' WITH GRANT OPTION"
 echo "========================================================================"
 echo "You can now connect to this MariaDB Server using:"
 echo ""
-echo "    mysql -u$ID -p$PASS -h<host> -P<port>"
+if [ $MARIADB_PASS ]
+then
+    echo "    mysql -u$ID -p<MARIADB_PASS> -h<host> -P<port>"
+else
+    echo "    mysql -u$ID -p$PASS -h<host> -P<port>"
+fi
 echo ""
 echo "Please remember to change the above password as soon as possible!"
 echo "MariaDB user 'root' has no password but only allows local connections"
